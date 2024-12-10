d6c4416d6c8a83b40b2944d775f5d1a52bf0c172
jefferson.willian <jefferson.lsilva@me.com.br>
2024-12-10 09:01:38 -0300

    new prompt structure

--- src/controller/geminiController.ts
+++ src/controller/geminiController.ts
@@ -15,15 +15,36 @@ class GeminiController {
 
     const prompt = `Crie uma receita para o ${optionMeal} 
         apenas com os seguintes ingredientes: ${ingredients}
-        e retorne em formato HTML com os titulos com font-weight 600 e o resto do testo co font-weight 400.
-        A fonte de todo conteúdo incluindo o título deve ter 16px.
-        Deve ter um título e uma breve introdução do prato.
-        Após a cada seção deixar um margin de 16px
-        No início deixar um margin top de 32px, este estilo deve ficar no h1
-        O titulo deve ser h1 e é importanto que tenha o font-size de 16px.
-        Do not give me then format as rich text, just plain html. Eg no '''html'''.
-        Start the response with <!DOCTYPE html>, not '''html
-        `;
+        e retorne um objeto com o objeto de titulo, objeto de introdução do prato, 
+        um objeto com os ingredientes, objeto com o modo de preparo, e objeto com observacoes,
+        siga o padrao: 
+        {
+          "titulo": "titulo",
+          "introducao": "introducao",
+          "ingredientes": [
+            {
+              "nome": "nome ingrediente",
+              "quantidade": "1 unidade"
+            },
+            {
+              "nome": "nome ingrediente",
+              "quantidade": "5 unidade"
+            }
+          ],
+          "modoDePreparo": [
+            "1. Aqueça uma frigideira em fogo médio.",
+            "2. Lorem ipsum
+            "3. Lorem ipsum
+            "4. Lorem ipsum
+            "5. Lorem ipsum
+            "9. Lorem ipsum"
+          ],
+          "observacoes": [
+            "Lorem ipsum",
+            "Lorem ipsum",
+            "Lorem ipsum"
+          ]
+        }`;
     const result = await model.generateContent(prompt);
     return res.status(200).json(result.response.text());
   };
