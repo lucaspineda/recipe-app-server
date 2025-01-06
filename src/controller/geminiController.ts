ef1aefdeba2cc49a97ef5014825db3398dc39005
jefferson.willian <jefferson.lsilva@me.com.br>
2025-01-06 15:39:41 -0300

    fix api key and add try catch

--- src/controller/geminiController.ts
+++ src/controller/geminiController.ts
@@ -2,18 +2,20 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 class GeminiController {
   static generatePrompt = async (req, res) => {
-    if (Object.keys(req.body).length === 0) {
-      return res.status(400).json({ body: "body must not be empty" });
-    }
-    const genAI = new GoogleGenerativeAI(
-      "AIzaSyBxuVntQlSSR-NyhUQC6I0wN9GI4YahvG8"
-    );
 
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    try {
+      if (Object.keys(req.body).length === 0) {
+        return res.status(400).json({ body: "body must not be empty" });
+      }
+      const genAI = new GoogleGenerativeAI(
+        "AIzaSyBlyiYUioKr9r4o9PhZjnOM4ZvOjW20KXA"
+      );
 
-    const { optionMeal, ingredients } = req.body;
+      const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-    const prompt = `Crie uma receita para o ${optionMeal} 
+      const { optionMeal, ingredients } = req.body;
+
+      const prompt = `Crie uma receita para o ${optionMeal} 
         apenas com os seguintes ingredientes: ${ingredients}
         e retorne um objeto com o objeto de titulo, objeto de introdução do prato, 
         um objeto com os ingredientes, objeto com o modo de preparo, e objeto com observacoes,
@@ -45,8 +47,13 @@ class GeminiController {
             "Lorem ipsum"
           ]
         }`;
-    const result = await model.generateContent(prompt);
-    return res.status(200).json(result.response.text());
+      const result = await model.generateContent(prompt);
+      console.log("response sent");
+      return res.status(200).json(result.response.text());
+    } catch (error) {
+      console.log(error)
+      return res.status(500).json({ error: error.message });
+    }
   };
 }
 
