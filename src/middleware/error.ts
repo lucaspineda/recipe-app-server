5c0a5050e561668e08b85d0ac5ab6bb927504fba
lucaspineda <lucas.pineda@hotmail.com>
2025-01-27 00:36:51 -0300

    fix types

--- src/middleware/error.ts
+++ src/middleware/error.ts
@@ -1,14 +1,23 @@
 import ApiError from '../utils/api-errors'
+import { Application, Request, Response, NextFunction } from 'express';
 
-const errorMiddleware = async app => {
-
-    app.use(async (error, res, req, next) => {
-
-        if (error instanceof ApiError)
-            return res.status(res.status).json(res.msg)
-
-        return res.status(500).json('AN INTERNAL SERVER ERROR HAPPENED!')
-    })
+interface ErrorMiddleware {
+    (app: Application): void;
 }
+
+interface ErrorHandler {
+    (error: any, req: Request, res: Response, next: NextFunction): void;
+}
+
+const errorMiddleware: ErrorMiddleware = async (app) => {
+    const errorHandler: ErrorHandler = async (error, req, res, next) => {
+        if (error as any) {
+            return res.status(error.status).json({ message: error.message });
+        }
+        return res.status(500).json('AN INTERNAL SERVER ERROR HAPPENED!');
+    };
+
+    app.use(errorHandler);
+};
 
 export default errorMiddleware
--- src/services/server.ts
+++ src/services/server.ts
@@ -1,6 +1,6 @@
 import app from '../app';
 
-const port = process.env.PORT || 3003; // Use the injected PORT or default to 3003
+const port: number = parseInt(process.env.PORT!) || 3003; // Use the injected PORT or default to 3003
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
